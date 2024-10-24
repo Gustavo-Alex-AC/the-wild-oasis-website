bf8f01737f777a7ee8a1ba9151a93bebbf5c7a15
Gustavo Chitangua <gchitangua@gmail.com>
2024-10-24 15:56:45 +0100

    Altered_config_nextjs

---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Reservations",
+};
+
+export default function Page() {
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Gustavo
+    </h2>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nuvuqitrmthnabmdbdkr.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/sign/cabins-image/**",
+      },
+    ],
+  },
+  //output: "export",
+};
 
 export default nextConfig;
